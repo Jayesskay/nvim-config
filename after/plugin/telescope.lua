ae4659f1791187e7243ed45e357293d092593438
Jayesskay <jayeskdev@gmail.com>
2023-04-08 13:46:48 +0200

    Added Find in files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,12 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>o', builtin.find_files, {})
+
+-- Find git file
 vim.keymap.set('n', 'qo', builtin.git_files, {})
-vim.keymap.set('n', '<leader>', builtin.git_files, {})
+
+-- Find any file
+vim.keymap.set('n', '<leader>o', builtin.find_files, {})
+
+-- Find phrase in files
+vim.keymap.set('n', '\\f', function()
+    builtin.grep_string({ search = vim.fn.input('Grep > ') });
+end)
