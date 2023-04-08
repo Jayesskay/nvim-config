ec9263d9f2a34ea722f5a08ed6d35878958da141
Jayesskay <jayeskdev@gmail.com>
2023-04-08 13:53:10 +0200

    Added treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,18 @@
 return require('packer').startup(function(use) 
     use 'wbthomason/packer.nvim'
     use 'Jayesskay/darcula'
-    use { 'nvim-telescope/telescope.nvim', tag = '0.1.1', requires = { {'nvim-lua/plenary.nvim'} } }
+
+    use { 
+        'nvim-telescope/telescope.nvim', 
+        tag = '0.1.1', 
+        requires = { 
+            { 'nvim-lua/plenary.nvim' } 
+        } 
+    }
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+
 end)
